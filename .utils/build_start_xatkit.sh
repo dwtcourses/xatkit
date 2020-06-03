d3744ce7acb77827d912808c0e9425e3fb2d6b1e
Hamza <hamza.eddouibi@gmail.com>
2020-06-03 16:53:22 +0200

    update  build_start_xatkit.sh

--- .utils/build_start_xatkit.sh
+++ .utils/build_start_xatkit.sh
@@ -7,7 +7,7 @@ e() {
 main() {
 
     git submodule update --init --recursive
-    build.sh --all --skip-tests --product
+    ./build.sh --all --skip-tests --product
     cd build/bin
     ./start-xatkit-linux.sh ../examples/GreetingsBots/GreetingsBot/GreetingsBot.properties > /dev/null
 }
